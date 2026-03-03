acc3376c4d254f78dc15defb4874ce40e98f562f
RahulNayak6467 <rahulnayak9326@gmail.com>
2026-03-03 23:00:03 +0530

    Add Seperate Function to fetch history

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import Loader from "@/Components/Loader";
 import MiniChart from "@/Components/DashboardPage/MiniChart";
 import CandlestickChart from "@/Components/Charts/CandleStick";
-import { TrendingDown } from "lucide-react";
+import { TrendingDown, TrendingUp } from "lucide-react";
 import { useState } from "react";
 const apiKey = import.meta.env.VITE_FMP_API_KEY;
 
@@ -50,9 +50,10 @@ function Dashboard() {
   const fetchData = async () => {
     const to = new Date().toISOString().split("T")[0];
     const from = new Date();
-    from.setDate(from.getDate() - Math.trunc(checkHistory % 30));
-    from.setMonth(from.getMonth() - (Math.trunc(checkHistory / 30) % 12));
-    from.setFullYear(from.getFullYear() - Math.trunc(checkHistory / 365));
+    // from.setDate(from.getDate() - Math.trunc(checkHistory % 30));
+    // from.setMonth(from.getMonth() - (Math.trunc(checkHistory / 30) % 12));
+    // from.setFullYear(from.getFullYear() - Math.trunc(checkHistory / 365));
+    from.setMonth(from.getMonth() - 6);
     const fromStr = from.toISOString().split("T")[0];
     const res = await Promise.all([
       fetch(
@@ -85,14 +86,14 @@ function Dashboard() {
       }),
     );
 
-    const candlestickSeries = data[0].map((d) => ({
-      time: d.date,
-      open: d.open,
-      high: d.high,
-      low: d.low,
-      close: d.close,
-      volume: d?.volume,
-    }));
+    // const candlestickSeries = data[0].map((d) => ({
+    //   time: d.date,
+    //   open: d.open,
+    //   high: d.high,
+    //   low: d.low,
+    //   close: d.close,
+    //   volume: d?.volume,
+    // }));
     // const candlestickSeries = data[0].map((el) =>
     //   el.reverse().map((d) => {
     //     return {
@@ -105,14 +106,54 @@ function Dashboard() {
     //   }),
     // );
 
-    // console.log(requiredData);
-    console.log(candlestickSeries);
-    return { requiredData, candlestickSeries };
+    console.log(requiredData);
+    // console.log(candlestickSeries);
+    return requiredData;
   };
 
   const { data, isLoading: isLoadingChart } = useQuery({
-    queryKey: ["IndexesHistory", checkHistory],
+    queryKey: ["IndexesHistory"],
     queryFn: () => fetchData(),
+    staleTime: Infinity,
+    gcTime: 1000 * 60 * 60, // keep in cache for 1 hour
+    retry: false, // disable auto retry
+    refetchOnWindowFocus: false,
+  });
+
+  const fetchHistoryData = async (checkHistory) => {
+    const to = new Date().toISOString().split("T")[0];
+    const from = new Date();
+    from.setDate(from.getDate() - Math.trunc(checkHistory % 30));
+    from.setMonth(from.getMonth() - (Math.trunc(checkHistory / 30) % 12));
+    from.setFullYear(from.getFullYear() - Math.trunc(checkHistory / 365));
+    const fromStr = from.toISOString().split("T")[0];
+    try {
+      const response = await fetch(
+        ` https://financialmodelingprep.com/stable/historical-price-eod/full?symbol=^IXIC&from=${fromStr}&to=${to}&apikey=${import.meta.env.VITE_FMP_API_KEY}`,
+      );
+
+      if (!response.ok) {
+        console.log(`An error occured ${response.status}`);
+      }
+      const data = await response.json();
+
+      const candlestickSeries = data.reverse().map((d) => ({
+        time: d.date,
+        open: d.open,
+        high: d.high,
+        low: d.low,
+        close: d.close,
+        volume: d?.volume,
+      }));
+      return candlestickSeries;
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const { data: candleStickData, isLoading: HistoryLoader } = useQuery({
+    queryKey: ["getIndexHistory", checkHistory],
+    queryFn: () => fetchHistoryData(checkHistory),
     staleTime: Infinity,
     gcTime: 1000 * 60 * 60, // keep in cache for 1 hour
     retry: false, // disable auto retry
@@ -123,6 +164,9 @@ function Dashboard() {
     return <Loader />;
   }
   if (isLoadingChart) {
+    return <Loader />;
+  }
+  if (HistoryLoader) {
     return (
       <div className="flex items-center justify-center">
         <div className="w-6 h-6 border-2 border-[#30363D] border-t-green-500 rounded-full animate-spin" />
@@ -135,14 +179,14 @@ function Dashboard() {
       <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 px-20">
         <MiniChart
           stockData={stockData[0][0]}
-          data={data?.requiredData[0]}
+          data={data[0]}
           colors={{
             color: "#22c55e",
           }}
         />
         <MiniChart
           stockData={stockData[1][0]}
-          data={data?.requiredData[1]}
+          data={data[1]}
           colors={{
             color: "#38bdf8",
           }}
@@ -151,7 +195,7 @@ function Dashboard() {
         />
         <MiniChart
           stockData={stockData[2][0]}
-          data={data?.requiredData[2]}
+          data={data[2]}
           colors={{
             color: "#eab308",
           }}
@@ -160,7 +204,7 @@ function Dashboard() {
         />
         <MiniChart
           stockData={stockData[3][0]}
-          data={data?.requiredData[3]}
+          data={data[3]}
           colors={{ color: "#a78bfa" }}
           widthBar={200}
           heightBar={60}
@@ -173,12 +217,26 @@ function Dashboard() {
               <div className="flex justify-between items-center  p-4">
                 <div className="flex gap-4 items-center">
                   <h1 className="font-extrabold text-4xl px-1 py-0.5 text-text-secondary">
-                    NASDAQ
+                    {stockData[1][0]?.name}
                   </h1>
-                  <span className="text-xl text-text-secondary">22,668.21</span>
+                  <span className="text-xl text-text-secondary">
+                    {stockData[1][0]?.price?.toFixed(2)}
+                  </span>
                   <div className="flex gap-2 items-center">
-                    <span className="text-xl text-text-secondary">-0.92%</span>
-                    <TrendingDown className="text-negative" />
+                    <span
+                      className={
+                        stockData[1][0]?.changePercentage?.toFixed(2) > 0
+                          ? "text-xl text-positive"
+                          : "text-xl text-negative"
+                      }
+                    >
+                      {stockData[1][0]?.changePercentage?.toFixed(2)}%
+                    </span>
+                    {stockData[1][0]?.changePercentage > 0 ? (
+                      <TrendingUp className="text-positive" />
+                    ) : (
+                      <TrendingDown className="text-negative" />
+                    )}
                   </div>
                 </div>
                 <div className="border border-bg-elevated  flex gap-2 rounded-md">
@@ -245,10 +303,7 @@ function Dashboard() {
                 </div>
               </div>
               <div className="p-6 border-zinc-700 m-4 mb-8 border-2 rounded-2xl">
-                <CandlestickChart
-                  data={data.candlestickSeries}
-                  className="p-6"
-                />
+                <CandlestickChart data={candleStickData} className="p-6" />
               </div>
             </div>
           </div>
